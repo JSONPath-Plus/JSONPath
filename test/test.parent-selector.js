c55d03296c5e5da489daec0a31fc0e70f6263c68
Robert Krahn <robert.krahn@gmail.com>
2013-10-25 04:15:11 -0700

    test cleanup

--- test/test.parent-selector.js
+++ test/test.parent-selector.js
@@ -1,46 +1,12 @@
-var jsonpath = require("../").eval
-  , testCase = require('nodeunit').testCase
+var jsonpath = require("../").eval,
+    testCase = require('nodeunit').testCase
 
-// tests based on examples at http://goessner.net/articles/JsonPath/
-
-var json = {"store": {
-    "book": [
-      { "category": "reference",
-        "author": "Nigel Rees",
-        "title": "Sayings of the Century",
-        "price": 8.95
-      },
-      { "category": "fiction",
-        "author": "Evelyn Waugh",
-        "title": "Sword of Honour",
-        "price": 12.99
-      },
-      { "category": "fiction",
-        "author": "Herman Melville",
-        "title": "Moby Dick",
-        "isbn": "0-553-21311-3",
-        "price": 8.99
-      },
-      { "category": "fiction",
-        "author": "J. R. R. Tolkien",
-        "title": "The Lord of the Rings",
-        "isbn": "0-395-19395-8",
-        "price": 22.99
-      }
-    ],
-    "bicycle": {
-      "color": "red",
-      "price": 19.95
-    }
-  }
-};
-var json2 = {
+var json = {
     "name": "root",
     "children": [
         {"name": "child1", "children": [{"name": "child1_1"},{"name": "child1_2"}]},
-        {"name": "child2", "children": [{"name": "child2_1"},{"name": "child2_2"}]},
-        {"name": "child3", "children": [{"name": "child3_1"}]},
-        {"name": "child4", "children": [{"name": "child4_1"}, {"name": "child4_3"}]}
+        {"name": "child2", "children": [{"name": "child2_1"}]},
+        {"name": "child3", "children": [{"name": "child3_1"}, {"name": "child3_2"}]}
     ]
 };
 
@@ -51,8 +17,8 @@ module.exports = testCase({
     "simple parent selection": function(test) {
     // ============================================================================
         test.expect(1);
-        var result = jsonpath(json, "$.store.book^", {wrap: false});
-        test.deepEqual(json.store, result);
+        var result = jsonpath(json, "$.children[0]^", {flatten: true});
+        test.deepEqual(json.children, result);
         test.done();
     },
 
@@ -60,18 +26,18 @@ module.exports = testCase({
     "parent selection with multiple matches": function(test) {
     // ============================================================================
         test.expect(1);
-        var expected = [json.store.book,json.store.book];
-        var result = jsonpath(json, "$.store.book[1:3]^");
+        var expected = [json.children,json.children];
+        var result = jsonpath(json, "$.children[1:3]^");
         test.deepEqual(expected, result);
         test.done();
     },
 
     // ============================================================================
-    "select matchingsiblings via parent": function(test) {
+    "select sibling via parent": function(test) {
     // ============================================================================
         test.expect(1);
-        var expected = [{"name": "child1_2"}];
-        var result = jsonpath(json2, "$..[?(@.name && @.name.match(/1_1$/))]^[?(@.name.match(/_2$/))]");
+        var expected = [{"name": "child3_2"}];
+        var result = jsonpath(json, "$..[?(@.name && @.name.match(/3_1$/))]^[?(@.name.match(/_2$/))]");
         test.deepEqual(expected, result);
         test.done();
     },
@@ -80,8 +46,8 @@ module.exports = testCase({
     "parent parent parent": function(test) {
     // ============================================================================
         test.expect(1);
-        var expected = json2.children[0].children;
-        var result = jsonpath(json2, "$..[?(@.name && @.name.match(/1_1$/))].name^^", {flatten: true});
+        var expected = json.children[0].children;
+        var result = jsonpath(json, "$..[?(@.name && @.name.match(/1_1$/))].name^^", {flatten: true});
         test.deepEqual(expected, result);
         test.done();
     },
@@ -90,7 +56,7 @@ module.exports = testCase({
     "no such parent": function(test) {
     // ============================================================================
         test.expect(1);
-        var result = jsonpath(json2, "name^^");
+        var result = jsonpath(json, "name^^");
         test.deepEqual([], result);
         test.done();
     }
