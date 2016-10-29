894da64b775e148c0720d207e561a7bb643b56b7
Brett Zamir <brettz9@yahoo.com>
2016-10-29 13:34:36 -0700

    Minor server tweak

--- test-helpers/nodeunit-server.js
+++ test-helpers/nodeunit-server.js
@@ -1,5 +1,5 @@
 require('http').createServer(function (req, res) {
-    var extra = req.url === '/test/' ? 'index.html' : '';
+    var extra = req.url === '/test/' ? 'index.html' : (req.url === '/test' ? '/index.html'  : '');
     var s = require('fs').createReadStream('.' + req.url + extra);
     s.pipe(res);
     s.on('error', function () {});
