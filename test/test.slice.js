5af0fc04411b23b749ea07b997e6110611e9bc87
Brett Zamir <brettz9@yahoo.com>
2020-01-13 16:52:31 +0800

    - Testing: Improve coverage

--- test/test.slice.js
+++ test/test.slice.js
@@ -8,4 +8,38 @@ describe('JSONPath - slice', function () {
         const result = jsonpath({json, path: '$.children[1:3]', wrap: false});
         assert.deepEqual(result, expected);
     });
+
+    it('should return objects with slice step', function () {
+        const jsonWithChildren = {
+            "name": "root",
+            "children": [
+                {a: 1}, {a: 2}, {a: 3}, {a: 4}, {a: 5}, {a: 6}
+            ]
+        };
+        const expected = [
+            {a: 2}, {a: 4}, {a: 6}
+        ];
+        const result = jsonpath({
+            json: jsonWithChildren,
+            path: '$.children[1:6:2]'
+        });
+        assert.deepEqual(result, expected);
+    });
+
+    it('should return objects with negative end slice', function () {
+        const jsonWithChildren = {
+            "name": "root",
+            "children": [
+                {a: 1}, {a: 2}, {a: 3}, {a: 4}, {a: 5}, {a: 6}
+            ]
+        };
+        const expected = [
+            {a: 2}, {a: 3}
+        ];
+        const result = jsonpath({
+            json: jsonWithChildren,
+            path: '$.children[1:-3]'
+        });
+        assert.deepEqual(result, expected);
+    });
 });
