0b39eb65463c9918434dec0424536b0cc0a3d2c7
Brett Zamir <brettz9@yahoo.com>
2020-01-13 19:44:43 +0800

    - Travis: Attempt to use patched mocha-multi-reporters to resolve Travis issue

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // Seemingly not being picked up (Node 8 only?), so just expressing on CL for now
-  // See https://github.com/stanleyhlng/mocha-multi-reporters/pull/61
-  // delay: true,
-  // reporter: 'mocha-multi-reporters'
+  delay: true,
+  reporter: 'mocha-multi-reporters'
 };
