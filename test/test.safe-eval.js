b2581f7ad5275a3bd36cb4e8b72dccc84c0bf642
Avinash Thakur <iamavinashthakur.at@gmail.com>
2024-11-17 01:17:02 +0530

    chore: remove unnecessary changes and rebuild docs
    rebuild docs using `pnpm run license-badges && pnpm run build-docs && pnpm run lint && pnpm run test`, remove unnecessary changes in test/test.safe-eval.js and badges/license-badge-dev.svg

--- test/test.safe-eval.js
+++ test/test.safe-eval.js
@@ -1,98 +1,77 @@
-import {checkBuiltInVMAndNodeVM} from "../test-helpers/checkVM.js";
+import {checkBuiltInVMAndNodeVM} from '../test-helpers/checkVM.js';
 
 checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
     describe(`JSONPath - Eval (${vmType} - safe)`, function () {
         before(setBuiltInState);
         const json = {
-            store: {
-                book: {
-                    category: "reference",
-                    author: "Nigel Rees",
-                    title: "Sayings of the Century",
-                    price: [8.95, 8.94]
+            "store": {
+                "book": {
+                    "category": "reference",
+                    "author": "Nigel Rees",
+                    "title": "Sayings of the Century",
+                    "price": [8.95, 8.94]
                 },
-                books: [
-                    {
-                        category: "fiction",
-                        author: "Evelyn Waugh",
-                        title: "Sword of Honour",
-                        price: [10.99, 12.29]
-                    },
-                    {
-                        category: "fiction",
-                        author: "Herman Melville",
-                        title: "Moby Dick",
-                        isbn: "0-553-21311-3",
-                        price: [8.99, 6.95]
-                    }
-                ]
+                "books": [{
+                    "category": "fiction",
+                    "author": "Evelyn Waugh",
+                    "title": "Sword of Honour",
+                    "price": [10.99, 12.29]
+                }, {
+                    "category": "fiction",
+                    "author": "Herman Melville",
+                    "title": "Moby Dick",
+                    "isbn": "0-553-21311-3",
+                    "price": [8.99, 6.95]
+                }]
             }
         };
 
-        it("multi statement eval", () => {
+        it('multi statement eval', () => {
             const expected = [json.store.books[0]];
-            const selector =
-                "$..[?(" +
-                "var sum = @.price && @.price[0]+@.price[1];" +
-                "sum > 20;)]";
-            const result = jsonpath({
-                json,
-                path: selector,
-                wrap: false,
-                eval: "safe"
-            });
+            const selector = '$..[?(' +
+                         'var sum = @.price && @.price[0]+@.price[1];' +
+                         'sum > 20;)]';
+            const result = jsonpath({json, path: selector, wrap: false, eval: 'safe'});
             assert.deepEqual(result, expected);
         });
-        it("multi statement eval (with use strict)", () => {
+        it('multi statement eval (with use strict)', () => {
             const expected = [json.store.books[0]];
-            const selector =
-                "$..[?(" +
-                '"use strict";' +
-                "var sum = @.price && @.price[0]+@.price[1];" +
-                "sum > 20;)]";
-            const result = jsonpath({
-                json,
-                path: selector,
-                wrap: false,
-                eval: "safe"
-            });
+            const selector = '$..[?(' +
+                         '"use strict";' +
+                         'var sum = @.price && @.price[0]+@.price[1];' +
+                         'sum > 20;)]';
+            const result = jsonpath({json, path: selector, wrap: false, eval: 'safe'});
             assert.deepEqual(result, expected);
         });
 
-        it("accessing current path", () => {
+        it('accessing current path', () => {
             const expected = [json.store.books[1]];
-            const result = jsonpath({
-                json,
-                path: "$..[?(@path==\"$['store']['books'][1]\")]",
-                wrap: false,
-                eval: "safe"
-            });
+            const result = jsonpath({json, path: "$..[?(@path==\"$['store']['books'][1]\")]", wrap: false, eval: 'safe'});
             assert.deepEqual(result, expected);
         });
 
-        it("sandbox", () => {
+        it('sandbox', () => {
             const expected = [json.store.book];
             const result = jsonpath({
                 json,
-                sandbox: {category: "reference"},
-                path: "$..[?(@.category === category)]",
-                wrap: false,
-                eval: "safe"
+                sandbox: {category: 'reference'},
+                path: "$..[?(@.category === category)]", wrap: false,
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
 
-        it("sandbox (with `arguments`)", () => {
+        it('sandbox (with `arguments`)', () => {
             const expected = [json.store.book];
             const selector = "$..[?(@.category === arguments)]";
             const result = jsonpath({
                 json,
                 path: selector,
                 sandbox: {
-                    arguments: "reference"
+                    arguments: 'reference'
                 },
                 wrap: false,
-                eval: "safe"
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
@@ -103,12 +82,11 @@ checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
                 json,
                 sandbox: {
                     category () {
-                        return "reference";
+                        return 'reference';
                     }
                 },
-                path: "$..[?(@.category === category())]",
-                wrap: false,
-                eval: "safe"
+                path: "$..[?(@.category === category())]", wrap: false,
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
@@ -119,123 +97,118 @@ checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
                 json,
                 sandbox: {
                     category () {
-                        return "reference";
+                        return 'reference';
                     }
                 },
-                path: "$..[?(@.category === category())]",
-                wrap: false,
-                eval: "safe"
+                path: "$..[?(@.category === category())]", wrap: false,
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
 
-        it("sandbox (with parsing function)", () => {
+        it('sandbox (with parsing function)', () => {
             const expected = [json.store.book];
             const result = jsonpath({
                 json,
                 sandbox: {
                     filter (arg) {
-                        return arg.category === "reference";
+                        return arg.category === 'reference';
                     }
                 },
-                path: "$..[?(filter(@))]",
-                wrap: false,
-                eval: "safe"
+                path: "$..[?(filter(@))]", wrap: false,
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
 
-        describe("cyclic object", () => {
+        describe('cyclic object', () => {
             // This is not an eval test, but we put it here for parity with item below
-            it("cyclic object without a sandbox", () => {
+            it('cyclic object without a sandbox', () => {
                 const circular = {a: {b: {c: 5}}};
                 circular.a.x = circular;
                 const expected = circular.a.b;
                 const result = jsonpath({
                     json: circular,
-                    path: "$.a.b",
+                    path: '$.a.b',
                     wrap: false,
-                    eval: "safe"
+                    eval: 'safe'
                 });
                 assert.deepEqual(result, expected);
             });
-            it("cyclic object in a sandbox", () => {
-                const circular = {category: "fiction"};
+            it('cyclic object in a sandbox', () => {
+                const circular = {category: 'fiction'};
                 circular.recurse = circular;
                 const expected = json.store.books;
                 const result = jsonpath({
                     json,
-                    path: "$..[?(@.category === aCircularReference.category)]",
+                    path: '$..[?(@.category === aCircularReference.category)]',
                     sandbox: {
                         aCircularReference: circular
                     },
                     wrap: false,
-                    eval: "safe"
+                    eval: 'safe'
                 });
                 assert.deepEqual(result, expected);
             });
         });
-        it("ternary operator in safe mode script", () => {
+        it('ternary operator in safe mode script', () => {
             const expected = [json.store.book, json.store.books[0]];
             const result = jsonpath({
                 json,
                 path: "$..[?(@ && @.price && ((@.price[0] + @.price[1]) > ((@.category === 'reference') ? 16 : 20)))]",
                 wrap: false,
-                eval: "safe"
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
-        it("unary operator in safe mode script", () => {
+        it('unary operator in safe mode script', () => {
             const expected = [json.store.book, json.store.books[1]];
             const result = jsonpath({
                 json,
-                path: "$..[?(@ && @.price && -10 < -(@.price[0]))]",
+                path: '$..[?(@ && @.price && -10 < -(@.price[0]))]',
                 wrap: false,
-                eval: "safe"
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
-        it("Array expression in safe mode script", () => {
+        it('Array expression in safe mode script', () => {
             const expected = [json.store.book, json.store.books];
             const result = jsonpath({
                 json,
                 path: '$..[?(["book", "books"].includes(@property))]',
                 wrap: false,
-                eval: "safe"
+                eval: 'safe'
             });
             assert.deepEqual(result, expected);
         });
-        describe("binary operators in safe mode script", () => {
+        describe('binary operators in safe mode script', () => {
             // eslint-disable-next-line no-shadow -- Convenient
             const json = {
-                store: {
-                    book: {
-                        category: "reference",
-                        author: "Nigel Rees",
-                        title: "Sayings of the Century",
-                        shelf: 1,
-                        price: [8.95, 8.94],
-                        meta: "12"
+                "store": {
+                    "book": {
+                        "category": "reference",
+                        "author": "Nigel Rees",
+                        "title": "Sayings of the Century",
+                        "shelf": 1,
+                        "price": [8.95, 8.94],
+                        "meta": '12'
                     },
-                    books: [
-                        {
-                            category: "fiction",
-                            author: "Evelyn Waugh",
-                            title: "Sword of Honour",
-                            shelf: 2,
-                            price: [10.99, 12.29],
-                            meta: 1073741822 // (-5) >>> 2
-                        },
-                        {
-                            category: "fiction",
-                            author: "Herman Melville",
-                            title: "Moby Dick",
-                            shelf: 3,
-                            isbn: "0-553-21311-3",
-                            price: [8.99, 6.95],
-                            emptyArray: []
-                        }
-                    ]
+                    "books": [{
+                        "category": "fiction",
+                        "author": "Evelyn Waugh",
+                        "title": "Sword of Honour",
+                        "shelf": 2,
+                        "price": [10.99, 12.29],
+                        "meta": 1073741822 // (-5) >>> 2
+                    }, {
+                        "category": "fiction",
+                        "author": "Herman Melville",
+                        "title": "Moby Dick",
+                        "shelf": 3,
+                        "isbn": "0-553-21311-3",
+                        "price": [8.99, 6.95],
+                        "emptyArray": []
+                    }]
                 }
             };
             const opPathExpecteds = [
@@ -266,24 +239,24 @@ checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
                         json,
                         path,
                         wrap: false,
-                        eval: "safe"
+                        eval: 'safe'
                     });
                     assert.deepEqual(result, expected);
                 });
             }
         });
 
-        describe("security tests", () => {
-            it("issue #226 1", () => {
+        describe('security tests', () => {
+            it('issue #226 1', () => {
                 assert.throws(() => {
                     jsonpath({
                         path: String.raw`$[?(var _$_root=[].constructor.constructor("console.log(this.process.mainModule.require(\"child_process\").execSync(\"id\").toString())");@root())]`,
-                        json: {a: "x"}
+                        json: {a: 'x'}
                     });
                 }, "Cannot read properties of  (reading 'constructor')");
             });
 
-            it("issue #226 2", () => {
+            it('issue #226 2', () => {
                 assert.throws(() => {
                     const pathDoS =
                         "$[?(con = constructor; dp = con.defineProperty; gopd = con.getOwnPropertyDescriptor; f = gopd(con, 'entries').value; alt = gopd(con.getPrototypeOf(f), 'apply'); dp(con.getPrototypeOf(_$_root.body), 'toString', alt);)]";
@@ -291,15 +264,15 @@ checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
                     const result = jsonpath({
                         json: {
                             referrer: {
-                                value: "https://authorized.com",
+                                value: 'https://authorized.com',
                                 writable: true
                             },
                             method: {
-                                value: "POST",
+                                value: 'POST',
                                 writable: true
                             },
                             body: {
-                                value: "Hello, World!",
+                                value: 'Hello, World!',
                                 writable: true
                             }
                         },
@@ -307,15 +280,15 @@ checkBuiltInVMAndNodeVM(function (vmType, setBuiltInState) {
                     });
 
                     result.toString(); // DoS
-                }, "constructor is not defined");
+                }, 'constructor is not defined');
             });
 
-            it("assignment to __proto__", () => {
+            it('issue #226 3', () => {
                 assert.throws(() => {
                     const path =
                         "$[?(__proto__ = 0; __proto__ = constructor; __proto__ = constructor; __proto__(\"process.mainModule.require('child_process').execSync('open /Users/andrea');\")())]";
                     jsonpath({json: {x: 1}, path});
-                }, "constructor is not defined");
+                }, 'constructor is not defined');
             });
         });
     });
