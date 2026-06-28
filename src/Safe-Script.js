1ed2a2c01c92191f9af062152ce3d86e07ac8fe1
Avinash Thakur <iamavinashthakur.at@gmail.com>
2026-06-29 01:34:30 +0530

    fix(eval): CVE: RCE in safe-eval
    Co-authored-by: Ryan Cruz <ryan@hackzero.ai>

--- src/Safe-Script.js
+++ src/Safe-Script.js
@@ -138,7 +138,7 @@ const SafeEval = {
             );
         }
         const result = obj[prop];
-        if (typeof result === 'function') {
+        if (typeof result === 'function' && result !== Function) {
             return result.bind(obj); // arrow functions aren't affected by bind.
         }
         return result;
@@ -162,12 +162,9 @@ const SafeEval = {
     evalCallExpression (ast, subs) {
         const args = ast.arguments.map((arg) => SafeEval.evalAst(arg, subs));
         const func = SafeEval.evalAst(ast.callee, subs);
-        /* c8 ignore start  */
         if (func === Function) {
-            // unreachable since BLOCKED_PROTO_PROPERTIES includes 'constructor'
             throw new Error('Function constructor is disabled');
         }
-        /* c8 ignore end  */
         return func(...args);
     },
     evalAssignmentExpression (ast, subs) {
